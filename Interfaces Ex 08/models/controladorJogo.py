94246b12baf2397aa4729c915aca27570536e8d9
Ana <85710984+bastosanaa@users.noreply.github.com>
2024-05-06 09:27:43 -0300

    Update controladorJogo.py

--- Interfaces Ex 08/models/controladorJogo.py
+++ Interfaces Ex 08/models/controladorJogo.py
@@ -1,75 +1,60 @@
 from AbstractControladorJogo import *
-import random
+from Personagem import Personagem
+from AbstractPersonagem import Tipo
+from Carta import Carta
+from Mesa import Mesa
 
 
 class ControladorJogo(AbstractControladorJogo):
     def __init__(self):
-        pass#implementar
+        self.__personagems = []
+        self.__baralho = []
 
-    '''
-    Retorna o baralho
-    @return o baralho
-    '''
     @property
     def baralho(self) -> list:
-        pass#implementar
+        return self.__baralho
 
-    '''
-    Retorna a lista de personagems
-    @return a lista de personagems
-    '''
     @property
     def personagems(self) -> list:
-        pass#implementar
+        return self.__personagems
 
-    '''
-    Permite incluir um novo Personagem na lista de personagens do jogo
-    @param energia Energia do novo Personagem
-    @param habilidade Habilidade do novo Personagem
-    @param velocidade Velocidade do novo Personagem
-    @param resistencia Resistencia do novo Personagem
-    @param tipo TipoPersonagem (Enum) do novo Personagem.
-    Deve ser utilizado TipoPersonagem.TIPO
-    @return Retorna o Personagem incluido na lista
-    '''
     def inclui_personagem_na_lista(self,
                                    energia: int,
                                    habilidade: int,
                                    velocidade: int,
                                    resistencia: int,
                                    tipo: Tipo) -> Personagem:
-        pass#implementar
+        personagem = Personagem(energia,
+                                habilidade,
+                                velocidade,
+                                resistencia,
+                                tipo)
+        self.__personagems.append(personagem)
+        return personagem
 
-    '''
-    Permite incluir uma nova Carta no baralho do jogo
-    @param personagem Personagem da nova carta que sera incluida
-    @return Retorna a Carta que foi incluida no baralho
-    '''
     def inclui_carta_no_baralho(self, personagem: Personagem) -> Carta:
-        pass#implementar
+        if isinstance(personagem, Personagem):
+            carta = Carta(personagem)
+            self.__baralho.append(carta)
+            return carta
+        return None
 
-    '''
-    Realiza uma jogada, ou seja:
-    1. Recebe a mesa onde estao as cartas lancadas pelo Jogador 1
-    e pelo Jogador 2
-     
-    2. Compara os valores totais das cartas dos jogadores que estao
-    na mesa
-    - O jogador que ganhar a rodada recebe a carta do jogador perdedor,
-    sendo assim ele deve chamar o metodo inclui_carta_na_mao para as
-    duas cartas que estao na mesa
-    - Caso ocorra empate ambos os jogadores devem chamar o metodo
-    inclui_carta_na_mao com suas respectivas cartas da mesa
-
-    3.Ao final do metodo o jogador que estiver com a mao vazia
-    perde o jogo (retornar o jogador vencedor). Caso ambos ainda tenham
-    cartas na mao retorne None para indicar que ninguem venceu o jogo.
-
-    @param mesa Mesa atual, contendo: Jogador 1, Jogador 2,
-    Carta do Jogador 1 e Carta do Jogador 2
-     
-    @return Retorna o Jogador vencedor da rodada.
-    Caso ocorra empate entre os jogadores, retorna None
-    '''
     def jogada(self, mesa: Mesa) -> Jogador:
-        pass#implementar
+        if isinstance(mesa, Mesa):
+            valor_carta_j1 = mesa.carta_jogador1.valor_total_carta()
+            valor_carta_j2 = mesa.carta_jogador2.valor_total_carta()
+            if valor_carta_j1 != valor_carta_j2:
+                if valor_carta_j1 > valor_carta_j2:
+                    mesa.jogador1.inclui_carta_na_mao(mesa.carta_jogador2)
+                    mesa.jogador1.inclui_carta_na_mao(mesa.carta_jogador1)
+                    if mesa.jogador2.mao == []:
+                        return mesa.jogador1
+                else:
+                    mesa.jogador2.inclui_carta_na_mao(mesa.carta_jogador1)
+                    mesa.jogador2.inclui_carta_na_mao(mesa.carta_jogador2)
+                    if mesa.jogador1.mao == []:
+                        return mesa.jogador2
+            else:
+                mesa.jogador2.inclui_carta_na_mao(mesa.carta_jogador2)
+                mesa.jogador1.inclui_carta_na_mao(mesa.carta_jogador1)
+                return None
