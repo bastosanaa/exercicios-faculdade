00a6f878aee09c86c9788b2d486b53a7045b84ac
Ana <85710984+bastosanaa@users.noreply.github.com>
2024-05-06 09:26:00 -0300

    Update mesa.py

--- Interfaces Ex 08/models/mesa.py
+++ Interfaces Ex 08/models/mesa.py
@@ -1,5 +1,6 @@
-from AbstractMesa import *
-from Carta import *
+from AbstractMesa import AbstractMesa
+from Carta import Carta
+from Jogador import Jogador
 
 
 class Mesa(AbstractMesa):
@@ -7,20 +8,27 @@ class Mesa(AbstractMesa):
     #Construtor fornecido, nao deve ser alterado
     def __init__(self, jogador1: Jogador, jogador2: Jogador,
                  cartaJogador1: Carta, cartaJogador2: Carta):
-        pass#implementar
+        if isinstance(jogador1, Jogador):
+            self.__jogador1 = jogador1
+        if isinstance(jogador2, Jogador):
+            self.__jogador2 = jogador2
+        if isinstance(cartaJogador1, Carta):
+            self.__cartajogador1 = cartaJogador1
+        if isinstance(cartaJogador2, Carta):
+            self.__cartajogador2 = cartaJogador2
 
     @property
     def jogador1(self) -> Jogador:
-        pass#implementar
+        return self.__jogador1
 
     @property
     def jogador2(self) -> Jogador:
-        pass#implementar
+        return self.__jogador2
 
     @property
     def carta_jogador1(self) -> Carta:
-        pass#implementar
+        return self.__cartajogador1
 
     @property
     def carta_jogador2(self) -> Carta:
-        pass#implementar
+        return self.__cartajogador2
